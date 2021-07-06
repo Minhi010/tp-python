9522e2e3049925408ee2e22caa634b1a7bd35254
Minh <lconlyentretenim@gmail.com>
2021-07-06 18:24:17 -0300

    cambios en botones y estilos

--- products/models.py
+++ products/models.py
@@ -7,7 +7,7 @@ import uuid
 
 class Product(models.Model):
    title = models.CharField(max_length=50)
-   description = models.TextField()
+   description = models.TextField(max_length=100)
    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.0)
    slug = models.SlugField(null=False, blank=False, unique=True)
    image=models.ImageField(upload_to='products/', null=False, blank=False)
